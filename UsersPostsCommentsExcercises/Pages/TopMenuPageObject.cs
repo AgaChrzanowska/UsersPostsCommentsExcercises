c18f942a600ac2458bc5746d2b848e1e4eb36058
diwadu <diwadu@gmail.com>
2019-09-02 16:35:50 +0200

    Top Menu tests.

--- UsersPostsCommentsExcercises/Pages/TopMenuPageObject.cs
+++ UsersPostsCommentsExcercises/Pages/TopMenuPageObject.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace UsersPostsCommentsExcercises.Pages
@@ -16,6 +17,8 @@ namespace UsersPostsCommentsExcercises.Pages
 
         [FindsBy(How = How.ClassName, Using = "nav-item")]
         public IList<IWebElement> Menu { get; set; }
+        [FindsBy(How = How.ClassName, Using = "navbar-brand")]
+        public IWebElement HomeLink { get; set; }
 
         public TopMenuPageObject(IWebDriver driver): base(driver)
         {
@@ -25,5 +28,26 @@ namespace UsersPostsCommentsExcercises.Pages
             int menuList = Menu.Count();
             return menuList;
         }
+        public AboutPageObject RedirectToAbout()
+        {
+            Thread.Sleep(3000);
+            IWebElement aboutMenuItem = Menu.ElementAt(2);
+            aboutMenuItem.Click();
+            return new AboutPageObject(Driver);
+        }
+
+        public PostsPageObject RedirectToPosts()
+        {
+            IWebElement postsMenuItem = Menu.ElementAt(1);
+            postsMenuItem.Click();
+            return new PostsPageObject(Driver);
+        }
+
+        public WelcomePageObject RedirectToHome()
+        {
+            HomeLink.Click();
+            return new WelcomePageObject(this.Driver);
+        }
+
     }
 }
--- UsersPostsCommentsExcercises/Tests/TopMenuTests.cs
+++ UsersPostsCommentsExcercises/Tests/TopMenuTests.cs
@@ -20,6 +20,91 @@ namespace UsersPostsCommentsExcercises.Tests
         }
 
         [TestMethod]
+        public void Go_To_About_From_Home()
+        {
+            try
+            {
+                Driver.Navigate().GoToUrl("http://localhost:4200/home");
+                AboutPageObject about = _topMenuPageObject.RedirectToAbout();
+                Assert.AreEqual("http://localhost:4200/about", Driver.Url);
+            }
+            catch (AssertFailedException afe)
+            {
+                FileLogHelper.Save(afe);
+            }
+        }
+
+        [TestMethod]
+        public void Go_To_Posts_From_Home()
+        {
+            try
+            {
+                Driver.Navigate().GoToUrl("http://localhost:4200/home");
+                PostsPageObject posts = _topMenuPageObject.RedirectToPosts();
+                Assert.AreEqual("http://localhost:4200/posts", Driver.Url);
+            }
+            catch (AssertFailedException afe)
+            {
+                FileLogHelper.Save(afe);
+            }
+        }
+        [TestMethod]
+        public void Go_To_About_From_Posts()
+        {
+            try
+            {
+                Driver.Navigate().GoToUrl("http://localhost:4200/posts");
+                AboutPageObject about = _topMenuPageObject.RedirectToAbout();
+                Assert.AreEqual("http://localhost:4200/about", Driver.Url);
+            }
+            catch (AssertFailedException afe)
+            {
+                FileLogHelper.Save(afe);
+            }
+        }
+        public void Go_To_Posts_From_Home_And_Check_H1_Text()
+        {
+            try
+            {
+                Driver.Navigate().GoToUrl("http://localhost:4200/home");
+                PostsPageObject posts = _topMenuPageObject.RedirectToPosts();
+                Assert.AreEqual("List of posts with authors", posts.H1Text.Text);
+            }
+            catch (AssertFailedException afe)
+            {
+                FileLogHelper.Save(afe);
+            }
+        }
+        public void Go_To_About_From_Posts_And_Check_H1_Text()
+        {
+            try
+            {
+                Driver.Navigate().GoToUrl("http://localhost:4200/posts");
+                AboutPageObject about = _topMenuPageObject.RedirectToAbout();
+                Assert.AreEqual("About author", Driver.Url);
+            }
+            catch (AssertFailedException afe)
+            {
+                FileLogHelper.Save(afe);
+            }
+        }
+
+        [TestMethod]
+        public void Go_To_Welcome_From_Posts()
+        {
+            try
+            {
+                Driver.Navigate().GoToUrl("http://localhost:4200/posts");
+                WelcomePageObject welcome = _topMenuPageObject.RedirectToHome();
+                Assert.AreEqual("http://localhost:4200/home", Driver.Url);
+            }
+            catch (AssertFailedException afe)
+            {
+                FileLogHelper.Save(afe);
+            }
+        }
+
+        [TestMethod]
         public void Check_List_Menu_Count()
         {
             try
@@ -33,7 +118,5 @@ namespace UsersPostsCommentsExcercises.Tests
                 FileLogHelper.Save(afe);
             }
         }
-
-        
     }
 }
